92b548f8b2d9e261b4fb9032a81d16a4cd45102b
Aman <amansoni20020@gmail.com>
2023-09-20 00:49:51 +0530

    fixed timing route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/Home/LandingPage";
-
 import DataContextProvider from "./components/context/DataContextProvider";
 import TimeOut from "./components/Data/TimeOut";
-
-import Timing from "./components/Data/Timing";
 function App() {
   return (
     <div className="App">
         <DataContextProvider>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/timing" element={<Timing />} />
           <Route path="/timeout" element={< TimeOut/>} />
         </Routes>
     </DataContextProvider>
